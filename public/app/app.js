dbda1f3071a5bd084cb2c1908ec98172f09e9a3e
nayab <nayab.s86@gmail.com>
2016-05-14 11:24:49 +0530

    protected admin pages via authorization on client side - refactored

--- public/app/app.js
+++ public/app/app.js
@@ -7,6 +7,22 @@
     window.app = angular.module('jedi', ['ngResource', 'ngRoute']);
 
     window.app.config(function ($routeProvider, $locationProvider) {
+
+        var routeRoleChecks = {
+            admin: {
+                auth: function (identityService, $q) {
+                    console.log('auth called');
+                    if (identityService.isAuthenticated() &&
+                        identityService.getCurrentUser().isAdmin()) {
+                        return true;
+                    }
+                    else {
+                        return $q.reject('Not Authorized');
+                    }
+                }
+            }
+        };
+
         $locationProvider.html5Mode(true);
         $routeProvider
             .when('/', {
@@ -15,10 +31,19 @@
             })
             .when('/admin/users', {
                 templateUrl: '/partials/admin/userList',
-                controller: 'userListController'
+                controller: 'userListController',
+                resolve: routeRoleChecks.admin
             });
     });
 
     window.app.value('appToastr', toastr);
+
+    window.app.run(function ($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function (event, current, previous, reason) {
+            if (reason === 'Not Authorized') {
+                $location.path('/');
+            }
+        })
+    })
 })();
 
